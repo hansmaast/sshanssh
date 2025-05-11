fce836a2384c5b12e43cadb3e3a4d0c5b20ed6a1
Hans Fredrik Maast <hfmaast@gmail.com>
2025-05-11 15:45:33 +0200

    add icone, change animations

--- components/GSappedContent.vue
+++ components/GSappedContent.vue
@@ -1,8 +1,9 @@
 <script setup lang="ts">
-import { useElementVisibility } from '@vueuse/core';
+import { useElementVisibility, useMediaQuery } from '@vueuse/core';
 import { gsap } from "gsap";
 import { SplitText } from "gsap/SplitText";
 import type { Content } from '~/content';
+import { Icon } from "@iconify/vue";
 
 gsap.registerPlugin(SplitText);
 
@@ -20,68 +21,98 @@ const containerIsVisible = useElementVisibility(containerTarget, {
 
 const titleClass = `title-${containerId}`
 const paragraphClass = `paragraph-${containerId}`
-const dotsClass = `dots-${containerId}`
+const dotClass = `dots-${containerId}`
+const iconClass = `icon-${containerId}`
 
 let timeline: gsap.core.Timeline | null = null
 onMounted(() => {
-    timeline = gsap.timeline().pause();
-    
-    const titleParts = SplitText.create(`.${titleClass}`, { type: "words, chars" });
-    const paragraphParts = SplitText.create(`.${paragraphClass}`, { type: "words, chars" });
+  timeline = gsap.timeline().pause();
 
-    timeline
-    .from(`.${dotsClass}`, {
-        duration: 0.25,
-        alpha: 0,
-        scale: 0,
-        stagger: 0.025,
-        ease: 'back.out',
-      })
+  const titleParts = SplitText.create(`.${titleClass}`, { type: "words, chars" });
+  const paragraphParts = SplitText.create(`.${paragraphClass}`, { type: "words, chars" });
+
+  timeline
     .fromTo(titleParts.chars, {
-        alpha: 0,
-        scale: .1,
-      },
-    {
+      alpha: 0,
+      scale: .1,
+    },
+      {
         duration: .1,
         alpha: 1,
         scale: 1,
         stagger: 0.03,
         ease: 'power3.inOut',
       })
-      .fromTo(paragraphParts.words, {
-        borderBottom: '0.75px solid transparent',
-        textDecoration: 'strike-through',
-      }, {
-        duration: .1,
-        stagger: 0.03,
-        ease: 'power3.inOut',
-        borderBottom: '0.75px solid black',
-        textDecoration: 'none',
-      })
+    .from(`.${iconClass}`, {
+      duration: .1,
+      alpha: 0,
+      scale: 0,
+      stagger: 0.03,
+      ease: 'power3.inOut',
+    })
+    .fromTo(paragraphParts.words, {
+      borderBottom: '0.75px solid transparent',
+      textDecoration: 'strike-through',
+    }, {
+      duration: .1,
+      stagger: 0.03,
+      ease: 'power3.inOut',
+      borderBottom: '0.75px solid black',
+      textDecoration: 'none',
+    })
 })
 
 watch(containerIsVisible, (isVisible) => {
   console.log('isVisible', isVisible)
-  
+
   if (isVisible && timeline) {
     timeline.play()
+    gsap.from(`.${dotClass}`, {
+      duration: 0.25,
+      alpha: 0,
+      scale: 0,
+      stagger: 0.025,
+      ease: 'back.out',
+    })
   }
 })
 </script>
 
 <template>
   <section class="max-w-2xl mx-auto min-h-svh flex flex-col justify-center">
-    <div :ref="containerId">
-      <component :is="content.id === 1 ? 'h1' : 'h2'" class="text-3xl md:text-4xl font-semibold mb-3"
-        :class="titleClass">
-        {{ content.title }}
-      </component>
-      <p class="text-lg md:text-xl font-light md:font-extralight mb-5" :class="paragraphClass">
-        {{ content.paragraph }}
-      </p>
-      <div class="flex gap-1" aria-hidden="true">
-        <div v-for="n in numberOfContentItems" :key="n" class="w-4 h-4 md:w-5 md:h-5 rounded-full shadow-sm"
-          :class="[n <= content.id ? 'bg-gray-900' : 'bg-gray-200', dotsClass]" />
+    <div 
+      :ref="containerId" 
+      class="flex flex-col gap-12 sm:flex-row sm:gap-5"
+      :class="{
+        'sm:flex-row-reverse': content.id % 2 === 0,
+      }"
+    >
+      <div class="flex-[3_1_0%]">
+        <component :is="content.id === 1 ? 'h1' : 'h2'" class="text-3xl md:text-4xl font-semibold mb-2 sm:mb-3"
+          :class="titleClass">
+          {{ content.title }}
+        </component>
+        <p class="text-lg md:text-xl font-light md:font-extralight mb-5" :class="paragraphClass">
+          {{ content.paragraph }}
+        </p>
+        <div class="flex gap-1" aria-hidden="true">
+          <div 
+            v-for="n in numberOfContentItems" :key="n" 
+            class="flex-1 h-1 md:h-1 rounded-sm shadow-sm md:shadow-md"
+            :class="[n <= content.id ? 'bg-gray-900' : 'bg-slate-100', dotClass]" 
+          />
+        </div>
+      </div>
+      <div class="flex-[1_1_0%] flex gap-3 items-center justify-center">
+        <div class="gap-12 sm:gap-6 grid grid-cols-2 place-items-center">
+            <Icon 
+              v-for="icon of content.icons" 
+              :key="icon" 
+              :icon="icon" 
+              class="w-14 h-14"
+              :class="iconClass"
+            />
+        </div>
       </div>
     </div>
   </section>
--- content.ts
+++ content.ts
@@ -1,48 +1,87 @@
-export type Image = {
-    src: string;
-    altText: string;
-};
-
 export type Content = {
     id: number;
     title: string;
     paragraph: string;
+    // Optional iconify icons, only compatible with iconify icons at the moment
+    icons?: string[];
 }
 
 export const content: Content[] = [
     {
         id: 1,
         title: 'Hi there, I\'m Hans.',
-        paragraph: 'Just a dude. Breathing, living and enjoying every aspect of this life.'
+        paragraph: 'Just a dude. Breathing, living and enjoying every aspect of this life.',
+        icons: [
+            'ph:sun-horizon-thin',
+            'ph:bicycle-thin',
+            'ph:tree-thin',
+            'ph:beer-stein-thin',
+        ]
     },
     {
         id: 2,
         title: 'Building simple, clean and accessible applications.',
-        paragraph: 'Striving for low complexity is an efficient way to make accessible applications, keeping users and maintainers happy. Simplicity scales, complexity fails.'
+        paragraph: 'Striving for low complexity is an efficient way to make accessible applications, keeping users and maintainers happy. Simplicity scales, complexity fails.',
+        icons: [
+            'ph:globe-thin',
+            'ph:gauge-thin',
+            'ph:devices-thin',
+            'ph:cube-focus-thin',
+        ]
     },
     {
         id: 3,
         title: 'Precision requires proper equipment.',
-        paragraph: 'Sharp minds choose sharp tools. A solid foundation of javascript, html and css makes developing for the web a breeze. Staying on top of the framework fatigue is easy when you master building with the basic blocks.'
+        paragraph: 'Sharp minds choose sharp tools. A solid foundation of javascript, html and css makes developing for the web a breeze. Staying on top of the framework fatigue is easy when you master building with the basic blocks.',
+        icons: [
+            'ph:brain-thin',
+            'ph:brackets-curly-thin',
+            'ph:compass-tool-thin',
+            'arcticons:emoji-brain',
+        ]
     },
     {
         id: 4,
         title: 'Accessibility for all.',
-        paragraph: 'Digital spaces should strive for inclusive design and functionality. Making accessible applications is beneficial for everyone, not just those who depend on it.'
+        paragraph: 'Digital spaces should strive for inclusive design and functionality. Making accessible applications is beneficial for everyone, not just those who depend on it.',
+        icons: [
+            'ph:person-arms-spread-thin',
+            'ph:smiley-thin',
+            'ph:users-four-thin',
+            'ph:person-simple-tai-chi-thin',
+        ]
     },
     {
         id: 5,
         title: 'Systematized design.',
-        paragraph: 'Know when to build and when to reuse. Sturdy foundations creates features for the future. Systematizing your design is essential for scaling up with speed and confidence.'
+        paragraph: 'Know when to build and when to reuse. Sturdy foundations creates features for the future. Systematizing your design is essential for scaling up with speed and confidence.',
+        icons: [
+            'ph:lego-thin',
+            'ph:tree-structure-thin',
+            'ph:codesandbox-logo-thin',
+            'ph:polygon-thin',
+        ]
     },
     {
         id: 6,
         title: 'Structured content, streamlined storytelling.',
-        paragraph: 'Well-organized bytes work wonders. Having a system that is customized for your needs is crucial for bringing your content to life. Get your message out the way you want.'
+        paragraph: 'Well-organized bytes work wonders. Having a system that is customized for your needs is crucial for bringing your content to life. Get your message out the way you want.',
+        icons: [
+            'ph:panorama-thin',
+            'ph:book-open-thin',
+            'ph:film-slate-thin',
+            'ph:bookmarks-simple-thin',
+        ]
     },
     {
         id: 7,
         title: 'Flow follows good plumbing.',
-        paragraph: 'Nobody likes a broken pipe — but a pipeline that fails fast beats a silent bug that breaks your uptime. A workflow that’s brittle by design lets us deploy with diligence, keeping a steady flow of features.'
+        paragraph: 'Nobody likes a broken pipe — but a pipeline that fails fast beats a silent bug that breaks your uptime. A workflow that’s brittle by design lets us deploy with diligence, keeping a steady flow of features.',
+        icons: [
+            'ph:line-segments-thin',
+            'ph:infinity-thin',
+            'ph:pipe-thin',
+            'ph:shuffle-thin',
+        ]
     }
 ]
